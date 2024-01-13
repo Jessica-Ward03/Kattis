8475e35643a29b8899f77aed68f5ce1e4a56a5ae
Jessica-Ward03 <jessicaward.jw03@gmail.com>
2024-01-13 09:37:16 -0500

    Job Expenses

---
+++ Job Expenses.cpp
@@ -0,0 +1,36 @@
+//
+// Created by jessi on 1/13/2024.
+//
+/*"At ACME Corp, the seller Robin is selling software like hotcakes. To sell the software, he arranges meetings all over the country. Of course, he always needs to travel there, and sometimes even stay the night. Other times, he decides that a hot dog on the way to a meeting counts as accommodation.
+
+Because Robin is very busy, he hasn’t been able to file his job expenses this month. Eret, the accountant, told him that he could fix it for him, as long as he got the price of all the expenses along with the receipts. Robin did this but misheard him: He has provided a list of all expenses and incomes he has brought in last month, with incomes as positive integers and expenses as negative integers.
+
+Can you help Eret find out how much expenses Robin has paid for the last month?
+
+Input
+The input consist of two lines: One line with the integer
+, and one line with
+ integers
+, representing the numbers Robin has written down.
+
+Output
+Output the sum of all the expenses Robin has paid for the last month.
+
+Limits
+ 0 <= N <= 20 000
+ -50 000 <= ki <= 500 000 and ki != 0 "
+
+ */
+#include <iostream>
+#include <valarray>
+
+using namespace std;
+
+
+string input;
+int main(){
+    cin>>input;
+    cout<<input<<endl;
+
+
+};
